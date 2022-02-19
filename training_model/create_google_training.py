5d609d5c14dc083c8ab9998000daf2b966d57edc
jwpetley <jwpetley@gmail.com>
2022-02-19 18:16:12 +0000

    Update to file making script

--- training_model/create_google_training.py
+++ training_model/create_google_training.py
@@ -6,18 +6,25 @@ fake_df = pd.read_csv("Fake.csv", index_col = None, header = 0,
 true_df = pd.read_csv("True.csv", index_col = None, header = 0,
     names = ['title', 'text', 'subject', 'date'])
 
-print(true_df)
+
 
 for i, row in true_df.iterrows():
-    print(row)
+
     sample_text = row['text']
     sample_title = row['title']
 
-    print(sample_text)
-
-
-
     sample_file = open("true_sample/sample_%i.txt"%i, 'w')
+    sample_file.write(sample_title + "\n")
+    sample_file.write(sample_text)
+    sample_file.close()
+
+for i, row in false_df.iterrows():
+
+    sample_text = row['text']
+    sample_title = row['title']
+
+
+    sample_file = open("false_sample/sample_%i.txt"%i, 'w')
     sample_file.write(sample_title + "\n")
     sample_file.write(sample_text)
     sample_file.close()
